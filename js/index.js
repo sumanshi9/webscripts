50f955d37629ccb241d9e8b2b437b92d4ecb6169
sumanshi9 <108696711+sumanshi9@users.noreply.github.com>
2024-02-20 09:52:53 -0700

    smooth scroll

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
 var acc = document.getElementsByClassName("accordion");
-            var i;
+var i;
 
-            for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var panel = this.nextElementSibling;
-                if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-                } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-                } 
-            });
-            }
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+}
+
+document.getElementById('primary-container').addEventListener('wheel', function(event) {
+if (event.deltaY > 0) {
+    event.target.scrollBy({
+    top: window.innerHeight,
+    left: 0,
+    behavior: 'smooth'});
+} else {
+    event.target.scrollBy({
+    top: -window.innerHeight,
+    left: 0,
+    behavior: 'smooth'});
+    }
+});
